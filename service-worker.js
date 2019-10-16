539f000246e232cc3004fc6448fe913c9078a91a
awehook <awehook@163.com>
2019-10-16 14:53:03 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-mindmap/precache-manifest.a54c5bf72ee4ebc1f27534e5ccd84f37.js"
+  "/react-mindmap/precache-manifest.8a02ab9414f5139c7869a65aa2437c06.js"
 );
 
 self.addEventListener('message', (event) => {
