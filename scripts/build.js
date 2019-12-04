d890963f532af95524e1084ea5fb5f9be7379606
awehook <awehook@163.com>
2019-12-04 19:17:16 +0800

    chore: webpack split chunks

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,7 @@
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+// process.env.NODE_ENV = 'production';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -37,6 +37,13 @@ const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
 const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
 
 const isInteractive = process.stdout.isTTY;
+
+if(fs.existsSync('build-old')) {
+  fs.removeSync('build-old')
+}
+if(fs.existsSync('build')) {
+  fs.renameSync('build','build-old');
+}
 
 // Warn and crash if required files are missing
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
@@ -132,7 +139,7 @@ function build(previousFileSizes) {
     console.log();
   }
 
-  console.log('Creating an optimized production build...');
+  console.log(`Creating an ${process.env.NODE_ENV} build...`);
 
   const compiler = webpack(config);
   return new Promise((resolve, reject) => {
