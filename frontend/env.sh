feeaf1dbaa1d2a99e939fe3cd33cb8c4e6953be2
catwang01 <edwardelricwzx@gmail.com>
2022-10-03 17:18:41 +0800

    support runtime environment varaibles

---
+++ frontend/env.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# line endings must be \n, not \r\n !
+echo "window.__env__ = {" > ./env-config.js
+awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> ./env-config.js
+echo "}" >> ./env-config.js
