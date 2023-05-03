9650839fe41d21e70c2c7619d4dd684162a8d0ee
catwang01 <edwardelricwzx@gmail.com>
2023-05-03 01:45:46 +0000

    1. fix the bug of not correctly using content; 2. add modals; 3. modify some variables' names

--- frontend/src/plugins/CreateJupyterNotebookPlugin/index.jsx
+++ frontend/src/plugins/CreateJupyterNotebookPlugin/index.jsx
@@ -1,16 +1,115 @@
-import React from 'react';
-import { ensureSuffix } from './utils';
-import { MenuItem, MenuDivider } from '@blueprintjs/core';
-import { JupyterClient } from './jupyter';
-import { v4 as uuidv4 } from 'uuid';
+import { BlockType, OpType as StandardOpType, FocusMode as StandardFocusMode } from '@blink-mind/core';
+import { Button, Dialog, MenuDivider, MenuItem } from '@blueprintjs/core';
 import { Map as ImmutableMap } from 'immutable';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import '../../icon/index.css';
-import { log } from './logger';
 import { JUPYTER_BASE_URL, JUPYTER_CLIENT_ENDPOINT, JUPYTER_CLIENT_TYPE, JUPYTER_ROOT_FOLDER } from './constant';
+import { JupyterClient } from './jupyter';
+import { log } from './logger';
+import { ensureSuffix } from './utils';
 
 const getIcon = () => {
     return <div className="icon-jupyter"></div>
 }
+
+export const OpType = {
+    CREATE_ASSOCIATED_JUPYTER_NOTE: 'CREATE_ASSOCIATED_JUPYTER_NOTE',
+    DELETE_ASSOCIATED_JUPYTER_NOTE: "DELETE_ASSOCIATED_JUPYTER_NOTE",
+    NOTIFY_REMOVED_JUPYTER_NOTE: "NOTIFY_REMOVED_JUPYTER_NOTE"
+}
+
+export const FocusMode = {
+    REMOVING_JUPYTER_NOTEBOOK: "REMOVING_JUPYTER_NOTEBOOK",
+    NOTIFY_REMOVED_JUPYTER_NOTE: "NOTIFY_REMOVED_JUPYTER_NOTE"
+}
+
+const renderModalRemovingJuyterNotebook = (props) => {
+    const { controller } = props;
+    const onClickYes = () => {
+        controller.run("operation", {
+            ...props,
+            opArray: [
+                {
+                    opType: OpType.DELETE_ASSOCIATED_JUPYTER_NOTE
+                },
+                {
+                    opType: StandardOpType.SET_FOCUS_MODE,
+                    focusMode: OpType.NOTIFY_REMOVED_JUPYTER_NOTE
+                }
+            ]
+        })
+    }
+    const onClickNo = () => {
+        controller.run('operation', {
+            ...props,
+            opType: StandardOpType.SET_FOCUS_MODE,
+            focusMode: StandardFocusMode.NORMAL
+        })
+    }
+
+    const dialogProps = {
+        key: "renderModalRemovingJuyterNotebook",
+        isOpen: true,
+        children: <>
+            { "Do you want to remove the attached jupyter note?" }
+            <Button onClick={ onClickYes }>Yes</Button>
+            <Button onClick={ onClickNo }>No</Button> 
+        </>
+    }
+    return <Dialog {...dialogProps} />
+}
+
+const renderModalNotifyRemovedJupyterNote = (props) => {
+    const { controller } = props;
+    const onClickYes = () => {
+        // controller.run("operation", {
+        //     ...props,
+        //     opArray: [
+        //         {
+        //             opType: OpType.DELETE_ASSOCIATED_JUPYTER_NOTE
+        //         },
+        //         {
+        //             opType: StandardOpType.SET_FOCUS_MODE,
+        //             focusMode: StandardFocusMode.NORMAL
+        //         }
+        //     ]
+        // })
+        controller.run("operation", {
+            ...props,
+            opType: OpType.DELETE_ASSOCIATED_JUPYTER_NOTE
+        })
+        controller.run("operation", {
+            ...props,
+            opType: StandardOpType.SET_FOCUS_MODE,
+            model: controller.currentModel,
+            focusMode: StandardFocusMode.NORMAL
+        })
+    }
+
+    const dialogProps = {
+        key: "renderModalNotifyRemovedJupyterNote",
+        isOpen: true,
+        children: <>
+            { "The jupyter note has been remove!" }
+            <Button onClick={ onClickYes }>Yes</Button>
+        </>
+    }
+    return <Dialog {...dialogProps} />
+}
+
+const focusModeCallbacks = new Map([
+    [FocusMode.REMOVING_JUPYTER_NOTEBOOK, renderModalRemovingJuyterNotebook],
+    [FocusMode.NOTIFY_REMOVED_JUPYTER_NOTE, renderModalNotifyRemovedJupyterNote]
+])
+
+const getNoteText = (controller, model, topicKey) => {
+    const topic = model.getTopic(topicKey);
+    const block = topic.getBlock(BlockType.CONTENT).block;
+    const noteTitle = controller.run('serializeBlockData', { controller, model, block });
+    return noteTitle;
+}
+
 
 export function CreateJupyterNotebookPlugin()
 {
@@ -19,15 +118,16 @@ export function CreateJupyterNotebookPlugin()
         rootFolder: JUPYTER_ROOT_FOLDER,
         clientType: JUPYTER_CLIENT_TYPE
     });
+
   return {
     getOpMap: function(props, next) {
         const opMap = next();
         const { jupyter_notebook_path, topicKey } = props;
-        opMap.set("CREATE_ASSOCIATED_JUPYTER_NOTE", ({ model }) => { 
+        opMap.set(OpType.CREATE_ASSOCIATED_JUPYTER_NOTE, ({ model }) => { 
             const newModel = model.setIn(["extData", "jupyter", topicKey, "path"], jupyter_notebook_path)
             return newModel; 
         });
-        opMap.set("DELETE_ASSOCIATED_JUPYTER_NOTE", ({ model }) => { 
+        opMap.set(OpType.DELETE_ASSOCIATED_JUPYTER_NOTE, ({ model }) => { 
             const newModel = model.deleteIn(['extData', 'jupyter', model.focusKey]);
             return newModel; 
         });
@@ -45,8 +145,7 @@ export function CreateJupyterNotebookPlugin()
     },
     customizeTopicContextMenu: function(props, next) {
         log("customizeTopicContextMenu")
-        log("parameters: ")
-        log({ props })
+        log("parameters: ", props)
 
         const { topicKey, model, controller } = props;
 
@@ -59,9 +158,8 @@ export function CreateJupyterNotebookPlugin()
             }
             const jupyter_notebook_id = uuidv4()
             const jupyter_notebook_path = jupyter_notebook_id + '/' + ensureSuffix(jupyter_notebook_id, ".ipynb")
-            
-            const noteTitle = model.getTopic(topicKey).content
-            jupyterClient.createNote(jupyter_notebook_path, noteTitle)
+            const text = getNoteText(controller, model, topicKey)
+            jupyterClient.createNote(jupyter_notebook_path, text)
                 .then(isSuccess => {
                     if (isSuccess)
                     {
@@ -69,8 +167,9 @@ export function CreateJupyterNotebookPlugin()
                             ...props,
                             topicKey,
                             jupyter_notebook_path: jupyter_notebook_path,
+                            // hack: if no use controller.currentModel, the topic may not correctly be focused
                             model: controller.currentModel,
-                            opType: 'CREATE_ASSOCIATED_JUPYTER_NOTE'
+                            opType: OpType.CREATE_ASSOCIATED_JUPYTER_NOTE
                         })
                     }
                 });
@@ -92,9 +191,13 @@ export function CreateJupyterNotebookPlugin()
         }
 
         const onClickRemoveJupyterNoteItem = () => {
-            controller.run("operation", {
+            const { controller } = props;
+            controller.run('operation', {
                 ...props,
-                opType: "DELETE_ASSOCIATED_JUPYTER_NOTE"
+                opType: StandardOpType.SET_FOCUS_MODE,
+                // hack: if no use controller.currentModel, the topic may not correctly be focused
+                model: controller.currentModel,
+                focusMode: FocusMode.REMOVING_JUPYTER_NOTEBOOK,
             })
         }
 
@@ -133,6 +236,28 @@ export function CreateJupyterNotebookPlugin()
           { associatedWithJupyterNote && removeJupyterNoteItem }
       </>;
     },
+
+    renderModal: (props, next) => {
+        const { model: { focusMode } } = props;
+        const res = next();
+        if (!focusModeCallbacks.has(focusMode))
+            return res;
+
+        const newRes = focusModeCallbacks.get(focusMode)(props)
+
+        return res === null ? [ newRes ] : [ ...res, newRes ]
+    },
+
+    getAllowUndo(props, next) {
+        const res = next();
+        if (!res)
+            return false;
+        const { model, focusMode } = props;
+        if (FocusMode.hasOwnProperty(model.focusMode) || FocusMode.hasOwnProperty(focusMode))
+            return false;
+        return res;
+    },
+
     deserializeExtData: (props, next) => {
       const extData = next();
       let newExtData = extData;
